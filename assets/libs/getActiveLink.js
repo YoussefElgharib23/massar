40fce5274b1204aae87c939d17b33c6c215e2c4e
Youssef El Gharib <elgharib.youssef27@gmail.com>
2021-06-29 19:13:25 +0100

    Fix more front problems

--- assets/libs/getActiveLink.js
+++ assets/libs/getActiveLink.js
@@ -3,7 +3,6 @@ $(document).ready(() => {
     let url = window.location.pathname
 
     $('.nav-link').each((i, item) => {
-        console.log($(item).attr('href'))
         if ($(item).attr('href') === url) {
             $(item).addClass('active');
         }
